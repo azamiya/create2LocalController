97649553fd3253230a2a70d66ca2f43aa4bc22f1
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-10-05 13:39:31 +0900

    create irobotCommand.js

---
+++ irobotConnect.js
@@ -0,0 +1,94 @@
+function start(io){
+	var irobotCommand = io.of('/irobotCommand').on('connection',function(socket){
+	socket.on('message', function(data) {
+		console.log(data);
+		commands[data.id]();
+	});
+
+	const path = getAvailableDevicePaths()[0];
+	if (!path) {
+	  return "Device cannot use";
+	}
+	debug(`connect to ${path}`);
+	const options = {
+	  baudRate: 115200,
+	  dataBits:8,
+	  parity:'none', stopBits:1,
+	  flowControl:false
+	};
+	const port = new SerialPort(path, options);
+
+	let inputBuffer = null;
+
+	port.on("data", (data) => {
+	  if (!inputBuffer) {
+	    inputBuffer = data;
+	  } else {
+	    inputBuffer = Buffer.concat([inputBuffer, data]);
+	  }
+
+	  debug(`${inputBuffer.length} Bytes received`);
+	  debug(inputBuffer.toJSON().data.toString());
+
+	  if (inputBuffer.length == 80) {
+	    debug("===== Clear");
+	    inputBuffer = null;
+	  }
+	});
+
+	port.on("open", () => {
+	  debug("connected");
+	  main();
+	});
+
+	function close() {
+	  debug("close");
+	  port.close();
+	  process.exit(0);
+	}
+
+
+	function drive(right, left) {
+	  const buf = new Buffer(5);
+	  buf[0] = 145;
+	  buf.writeInt16BE(right, 1);
+	  buf.writeInt16BE(left, 3);
+	  port.write(buf);
+	  debug(buf.toJSON().data.toString());
+	}
+
+	function main() {
+	    port.write(Buffer.from([128]));
+	    port.write(Buffer.from([131]));
+	}
+
+	  var commands=[
+	    () => { drive(-20, -20); setTimeout(() => { drive(0,0) }, 500)},
+	    () => { drive(20, 20); setTimeout(() => { drive(0,0) }, 500)},
+	    () => { drive(-20, 20); setTimeout(() => { drive(0,0) }, 500)},
+	    () => { drive(20, -20); setTimeout(() => { drive(0,0) }, 500)},
+	    () => { port.write(Buffer.from([7])) },
+	    () => { port.write(Buffer.from([132])) },
+	    () => { port.write(Buffer.from([131])) },
+	    () => { port.write(Buffer.from([128])) },
+	    () => { port.write(Buffer.from([139, 0, 125, 255])) },
+	    () => { port.write(Buffer.from([139, 0, 0, 0])) },
+	    //(arr) =>{ port.write(Buffer.from(arr)) },
+	    () => { port.write(Buffer.from([140, 3, 1, 64, 16, 141, 3])) },
+	    //(str) => {var a = str.split("").map((c) => {return  c.charCodeAt();}); a.unshift(164); port.write(Buffer.from(a)); },
+	    //drive,
+	    () => { close() }
+	 ];
+
+
+	function getAvailableDevicePaths() {
+	  const paths = fs.readdirSync("/dev").map((d) => {
+	    if (d.includes("tty.usb")) {
+	      return "/dev/" + d;
+	    }
+	  }).filter(Boolean);
+	  return paths;
+	}
+}
+
+exports.start = start;
